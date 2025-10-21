4fd98448c5a16fdeab8bb3bad8714d86dea3cbb6
Matteo <MATTEO.CIANELLI@correo.ucu.edu.uy>
2025-10-21 12:03:04 -0300

    Test de MainFacade completados y solucion de problemas

--- test/Library.Tests/MainFacadeTests.cs
+++ test/Library.Tests/MainFacadeTests.cs
@@ -36,12 +36,76 @@ public class MainFacadeTests
     [Test]
     public void DeleteClientTest()
     {
+        // Arrange
+        MainFacade mainFacade = new MainFacade();
+        Seller jose = new Seller("Jose");
+        Client mario = mainFacade.CreateClient("Mario", "Dias", "abcdefg", "123456789", Client.GenderType.male, "19/03/2000", jose);
+        Client mariano = mainFacade.CreateClient( "Mariano", "Dominguez", "gfedcba", "987654321", Client.GenderType.male, "02/12/1990", jose);
         
+        // Act
+        mainFacade.DeleteClient(0);
+        
+        //Assert
+        Assert.That(mainFacade.GetClients().Count, Is.EqualTo(1));
     }
 
     [Test]
     public void SerchClientByNameTest()
     {
-        
+        MainFacade mainFacade = new MainFacade();
+        Seller jose = new Seller("Lucas");
+        Client mario = mainFacade.CreateClient("Mario", "Dias", "abcdefg", "123456789", Client.GenderType.male, "19/03/2000", jose);
+        Client mariano = mainFacade.CreateClient("Mariano", "Dominguez", "gfedcba", "987654321", Client.GenderType.male, "02/12/1990", jose);
+        List<Client> expected = new List<Client>();
+        expected.Add(mario);
+        // Act
+        List<Client> actual = mainFacade.SearchClientByName("Mario");
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void SerchClientByLastNameTest()
+    {
+        MainFacade mainFacade = new MainFacade();
+        Seller jose = new Seller("Lucas");
+        Client mario = mainFacade.CreateClient("Mario", "Dias", "abcdefg", "123456789", Client.GenderType.male, "19/03/2000", jose);
+        Client mariano = mainFacade.CreateClient("Mariano", "Dominguez", "gfedcba", "987654321", Client.GenderType.male, "02/12/1990", jose);
+        List<Client> expected = new List<Client>();
+        expected.Add(mariano);
+        // Act
+        List<Client> actual = mainFacade.SearchClientByLastName("Dominguez");
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void SerchClientByEmailTest()
+    {
+        MainFacade mainFacade = new MainFacade();
+        Seller jose = new Seller("Lucas");
+        Client mario = mainFacade.CreateClient("Mario", "Dias", "abcdefg", "123456789", Client.GenderType.male, "19/03/2000", jose);
+        Client mariano = mainFacade.CreateClient("Mariano", "Dominguez", "gfedcba", "987654321", Client.GenderType.male, "02/12/1990", jose);
+        List<Client> expected = new List<Client>();
+        expected.Add(mario);
+        // Act
+        List<Client> actual = mainFacade.SearchClientByEmail("abcdefg");
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void SerchClientByPhoneTest()
+    {
+        MainFacade mainFacade = new MainFacade();
+        Seller jose = new Seller("Lucas");
+        Client mario = mainFacade.CreateClient("Mario", "Dias", "abcdefg", "123456789", Client.GenderType.male, "19/03/2000", jose);
+        Client mariano = mainFacade.CreateClient("Mariano", "Dominguez", "gfedcba", "987654321", Client.GenderType.male, "02/12/1990", jose);
+        List<Client> expected = new List<Client>();
+        expected.Add(mariano);
+        // Act
+        List<Client> actual = mainFacade.SearchClientByPhone("987654321");
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
     }
 }
