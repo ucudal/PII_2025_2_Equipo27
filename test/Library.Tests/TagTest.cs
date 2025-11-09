66c5c2c7c5c403262346c33b42f61ecbee1f0f7f
Ezequiel Pasto <ezequiel.pastorino@correo.ucu.edu.uy>
2025-11-09 20:53:04 -0300

    Test de las excepciones

--- test/Library.Tests/TagTest.cs
+++ test/Library.Tests/TagTest.cs
@@ -2,5 +2,22 @@ namespace Library.Tests;
 
 public class TagTest
 {
-    
+    /// <summary>
+    /// Verifica que el nombre de la etiqueta sea ingresado correctamente
+    /// </summary>
+    [Test]
+    public void SetTagName_Valid()
+    {
+        Tag tag = new Tag("VIP");
+        Assert.That("VIP", Is.EqualTo(tag.TagName));
+    }
+
+    /// <summary>
+    /// Verifica que no se lance una excepción si se crea una etiqueta sin nombre
+    /// </summary>
+    [Test]
+    public void SetTagName_Exception()
+    {
+        Assert.Throws<ArgumentException>(() => Is.EqualTo(new Tag("")));
+    }
 }
