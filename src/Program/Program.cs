2f1d92f553094cbbe6061be9d6bb89822fcd0d40
Facundo Gallinal <facundo.gallinal@correo.ucu.edu.uy>
2025-11-11 11:56:27 -0300

    Bot

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using Library;
+using Ucu.Poo.DiscordBot.Domain;
+using Ucu.Poo.DiscordBot.Services;
 
-namespace Program
+namespace Ucu.Poo.DiscordBot.Program
 {
-    class Program
+    /// <summary>
+    /// Un programa que implementa un bot de Discord.
+    /// </summary>
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Punto de entrada al programa.
+        /// </summary>
+        private static void Main(string [] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length != 0)
+            {
+                DemoFacade(args);
+            }
+            else
+            {
+                DemoBot();
+            }
+        }
+
+        private static void DemoFacade(string [] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine(AdminFacade.Instance.GetUserInfo(args[0]));
+            }
+        }
+
+        private static void DemoBot()
+        {
+            BotLoader.LoadAsync().GetAwaiter().GetResult();
         }
     }
 }
